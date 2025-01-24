3fdf7ef4063da7a99f991037ef2a4e15da0aa6b9
nanayawdev <nanayawisrael@gmail.com>
2025-01-24 14:20:55 +0000

    Refactor API constants: Replace hardcoded API URLs with environment variables for better configuration management. This change enhances flexibility and allows for easier updates to API endpoints without modifying the source code.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
-export const API_BASE_URL = 'https://api-server.krontiva.africa';
-export const GET_ORDERS_ENDPOINT = '/api:uEBBwbSs/delikaquickshipper_orders_table';
-export const GET_RESTAURANTS_ENDPOINT = '/api:uEBBwbSs/delikaquickshipper_restaurants_table';
-export const GET_BRANCHES_ENDPOINT = '/api:uEBBwbSs/delikaquickshipper_branches_table';
-export const GET_MENU_ENDPOINT = '/api:uEBBwbSs/delikaquickshipper_menu_table';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const GET_ORDERS_ENDPOINT = import.meta.env.VITE_GET_ORDERS_ENDPOINT;
+export const GET_RESTAURANTS_ENDPOINT = import.meta.env.VITE_GET_RESTAURANTS_ENDPOINT;
+export const GET_BRANCHES_ENDPOINT = import.meta.env.VITE_GET_BRANCHES_ENDPOINT;
+export const GET_MENU_ENDPOINT = import.meta.env.VITE_GET_MENU_ENDPOINT;
 export const GET_ALL_MENU_ENDPOINT = '/api:uEBBwbSs/get/all/menu/onboarding'; 
