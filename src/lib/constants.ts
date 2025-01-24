cb935c3f3242dfea30bdfd76c5a6c80907ae0b42
nanayawdev <nanayawisrael@gmail.com>
2025-01-24 14:24:25 +0000

    Refactor API constants: Replace hardcoded GET_ALL_MENU_ENDPOINT with an environment variable for improved configuration management. This change enhances flexibility and allows for easier updates to the API endpoint without modifying the source code.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const GET_ORDERS_ENDPOINT = import.meta.env.VITE_GET_ORDERS_ENDPOINT;
 export const GET_RESTAURANTS_ENDPOINT = import.meta.env.VITE_GET_RESTAURANTS_ENDPOINT;
 export const GET_BRANCHES_ENDPOINT = import.meta.env.VITE_GET_BRANCHES_ENDPOINT;
 export const GET_MENU_ENDPOINT = import.meta.env.VITE_GET_MENU_ENDPOINT;
-export const GET_ALL_MENU_ENDPOINT = '/api:uEBBwbSs/get/all/menu/onboarding'; 
+export const GET_ALL_MENU_ENDPOINT = import.meta.env.VITE_GET_ALL_MENU_ENDPOINT; 
